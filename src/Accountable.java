5741165e7ee0344a30ba01ef034bd5b0e6c18ca2
Chomusuke <yukhanov.artur@gmail.com>
2022-12-01 12:07:13 +0100

    basic buttons (no actions yet)

--- src/Accountable.java
+++ src/Accountable.java
@@ -1,9 +1,3 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /*  Accountable: a personal spending monitoring program
     Copyright (C) 2022  Artur Yukhanov
 
@@ -21,23 +15,55 @@ import javafx.stage.Stage;
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 public class Accountable extends Application {
+
+    private static final int MIN_WIDTH = 360;
+    private static final int MIN_HEIGHT = 640;
+    private static final int PAD = 16;
+    private static final String CATEGORIES = "Categories";
+    private static final String TAGS = "Tags";
+    private static final String ADD_TX = "Add Tx";
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Button b = new Button();
-        b.setText("Hello, and welcome to Accountable!");
+    public void start(Stage stage) {
 
-        StackPane root = new StackPane();
-        root.getChildren().add(b);
-        Scene scene = new Scene(root, 300, 250);
+        // Top buttons
+        HBox buttons = new HBox();
 
-        primaryStage.setTitle("Accountable");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Button categories = new Button(CATEGORIES);
+        Button tags = new Button(TAGS);
+        Button addTx = new Button(ADD_TX);
+        buttons.getChildren().addAll(categories, tags, addTx);
+
+        // Vertical app layout
+        SplitPane appLayout = new SplitPane(buttons);
+        appLayout.setOrientation(Orientation.HORIZONTAL);
+
+        BorderPane frame = new BorderPane();
+        frame.setCenter(appLayout);
+        frame.setMargin(appLayout, new Insets(PAD, PAD, PAD, PAD));
+        Scene scene = new Scene(frame, MIN_WIDTH, MIN_HEIGHT);
+
+        stage.setTitle("Accountable");
+        stage.setScene(scene);
+        stage.setWidth(MIN_WIDTH);
+        stage.setHeight(MIN_HEIGHT);
+        stage.setResizable(false);
+
+        stage.show();
     }
 }
