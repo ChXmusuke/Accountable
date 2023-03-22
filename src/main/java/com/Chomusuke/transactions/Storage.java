e5f9f5e50e01684737d379b05b5fb2ec319c2b90
Artur <yukhanov.artur@gmail.com>
2023-03-22 14:40:40 +0100

    Implemented storage of transactions

--- src/main/java/com/Chomusuke/transactions/Storage.java
+++ src/main/java/com/Chomusuke/transactions/Storage.java
@@ -1,5 +1,5 @@
 /*  Accountable: a personal spending monitoring program
-    Copyright (C) 2022  Artur Yukhanov
+    Copyright (C) 2023  Artur Yukhanov
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,137 +17,89 @@
 
 package com.chomusuke.transactions;
 
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
-import com.chomusuke.listDecorators.NumberedOrderedArray;
-import com.chomusuke.listDecorators.OrderedArray;
-import com.chomusuke.util.DateUtil;
-import com.chomusuke.util.Preconditions;
+import static com.chomusuke.transactions.Transaction.TransactionType;
+import static com.chomusuke.transactions.Transaction.ValueType;
 
 public class Storage {
 
-    final private static int MEM_CACHE_CAPACITY = 36;
-
-    // Memory cache
-    // Array structure: Memory<years<months>>
-    final private OrderedArray<NumberedOrderedArray<Transaction>> transactions;
+    private static final Path DIR_NAME = Path.of("/Users/artur/Library/Application Support/Accountable/storage/");
 
     /**
-     * Constructor for a storage structure.
-     * 
-     * Contains an array for the memory cache.
+     * Don't let anyone instantiate this class.
      */
-    public Storage() {
-        transactions = new OrderedArray<NumberedOrderedArray<Transaction>>(new ArrayList<>(MEM_CACHE_CAPACITY));
-    }
+    private Storage() {}
 
     /**
-     * Initializes a Storage instance with pre-existing transaction data.
-     * Mainly useful for testing.
-     * 
-     * @param transactions
-     *                     a transactions MemCache
+     * Writes the specified list to a file according to the given year and month.
+     *
+     * @param list  a {@code Transaction} list
+     * @param year  a value
+     * @param month a value
+     *
+     * @return {@code true} on success.
      */
-    public Storage(OrderedArray<NumberedOrderedArray<Transaction>> transactions) {
-        this.transactions = transactions;
-    }
+    public static boolean write(LinkedList<Transaction> list, int year, int month) {
 
-    /**
-     * Adds transaction {@code t} to the MemCache
-     * 
-     * @param t
-     *          a {@code Transaction}
-     */
-    public void addTransaction(Transaction t) {
-        // Finds the Month array to which the transaction must be added
-        int index = indexOfNumber(transactions, DateUtil.dateID(t.date()));
-        if (index >= 0)
-            transactions.get(index)
-                    .add(t);
-        else {
-            // If no such array exists, create one
-            // TODO: implement disk storage
-            NumberedOrderedArray<Transaction> newArray = new NumberedOrderedArray<>(DateUtil.dateID(t.date()));
-            newArray.add(t);
-            transactions.add(newArray);
+        Path dir = DIR_NAME.resolve(Integer.toString(year));
+        Path file = Path.of(Integer.toString(month));
+
+        try {
+            Files.createDirectories(dir);
+            Files.createFile(dir.resolve(file));
+        } catch (FileAlreadyExistsException e) {
+            System.out.println("file already exists");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
 
-    /**
-     * Removes the {@code Transaction} t if it is in the MemCache.
-     * 
-     * @param t
-     *          a {@code Transaction}
-     */
-    public void removeTransaction(Transaction t) {
-        /*
-         * Uses Binary Search to find a transaction with the same date as t.
-         * Since the transactions are ordered by date, and there may be several
-         * transactions with the same date, binary search is likely to fall in a suite
-         * of transactions with the same date.
-         */
-        NumberedOrderedArray<Transaction> mArray = readMonth(DateUtil.dateID(t.date()));
-        int i = Collections.binarySearch(mArray, t);
+        try (DataOutputStream output = new DataOutputStream(new FileOutputStream(dir.resolve(file).toString()))) {
+            output.writeInt(list.size());
 
-        /*
-         * If a Transaction with the same date as t is found,
-         * the algorithm looks into the TXs on both sides of the transaction to find one
-         * corresponding to t.
-         * If such a transaction is found, it is removed from the array
-         */
-        if (i >= 0) {
-            for (int d = 0; i + d < mArray.size() && mArray.get(i + d).date() == t.date(); d++)
-                if (mArray.get(i + d).equals(t))
-                    mArray.remove(i + d);
-            for (int d = 1; i - d >= 0 && mArray.get(i - d).date() == t.date(); d++) {
-                if (mArray.get(i - d).equals(t))
-                    mArray.remove(i - d);
+            for (Transaction t : list) {
+                output.writeUTF(t.name());
+                output.writeByte(t.to());
+                output.writeByte(t.packTypes());
+                output.writeFloat(t.value());
             }
-        }
-    }
-
-    /**
-     * Queries the given month's data from memory cache.
-     * 
-     * @param dateID
-     *               a dateID
-     * 
-     * @return A {@code NumberedArray} with the month's transaction data.
-     *         the {@code NumberedArray} is empty if the data has not been
-     *         found.
-     */
-    public NumberedOrderedArray<Transaction> readMonth(int dateID) {
-        // Only values with the dateID format are accepted
-        Preconditions.checkArgument(DateUtil.isDateID(dateID));
-
-        int index = Collections.binarySearch(transactions, new NumberedOrderedArray<>(null, dateID));
-        if (index >= 0)
-            return transactions.get(index);
-        else
-            return new NumberedOrderedArray<Transaction>(dateID);
-    }
-
-    /**
-     * If a {@code NumberedOrderedArray} with the given number is found,
-     * its index will be returned.
-     * 
-     * @param <E>
-     *              The type of object contained in the list. Not relevant to this
-     *              method.
-     * @param array
-     *              An array containing {@code NumberedOrderedArray}s
-     * @param n
-     *              the number to be searched
-     * 
-     * @return the index of a corresponding {@code NumberedOrderedArray} if found,
-     *         -1 if not found
-     */
-    private static <E extends Comparable<E>> int indexOfNumber(List<NumberedOrderedArray<E>> array, int n) {
-        for (int i = 0; i < array.size(); i++) {
-            if (array.get(i).getNumber() == n)
-                return i;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return -1;
+        return true;
+    }
+
+    public static LinkedList<Transaction> read(int year, int month) {
+        Path dir = DIR_NAME.resolve(Integer.toString(year));
+        Path file = Path.of(Integer.toString(month));
+
+        LinkedList<Transaction> txs = new LinkedList<>();
+
+        try (DataInputStream input = new DataInputStream(new FileInputStream(dir.resolve(file).toString()))) {
+            int entries = input.readInt();
+
+            for (int i = 0 ; i < entries ; i++) {
+                String name = input.readUTF();
+
+                byte to = input.readByte();
+
+                byte types = input.readByte();
+                TransactionType tt = TransactionType.of((byte) (types >>> 2));
+                ValueType vt = ValueType.of(types);
+
+                float value = input.readFloat();
+
+                txs.add(new Transaction(name, to, tt, vt, value));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return txs;
     }
 }
