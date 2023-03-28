00cbeb0074e2754d6041d42c278ae16b9aa560e0
Artur <yukhanov.artur@gmail.com>
2023-03-28 11:50:31 +0200

    GUI adjustments

--- src/main/java/com/Chomusuke/gui/stage/AddFileScreen.java
+++ src/main/java/com/Chomusuke/gui/stage/AddFileScreen.java
@@ -2,11 +2,13 @@ package com.chomusuke.gui.stage;
 
 import com.chomusuke.logic.Storage;
 import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
@@ -14,14 +16,20 @@ import javafx.util.converter.IntegerStringConverter;
 import java.util.ArrayList;
 
 public class AddFileScreen {
+
+    private static final int PADDING = 8;
+
     public static void show() {
         Stage stage = new Stage();
         stage.setResizable(false);
         VBox root = new VBox();
         root.getStyleClass().add("background");
+        root.setPadding(new Insets(PADDING));
+        root.setSpacing(PADDING);
         stage.setScene(new Scene(root));
 
         HBox inputs = new HBox();
+        inputs.setSpacing(PADDING);
 
         TextField year = new TextField();
         TextField month = new TextField();
@@ -48,12 +56,16 @@ public class AddFileScreen {
 
         Button submit = new Button("Ajouter");
         submit.setOnAction(a -> {
+            if (year.getText().equals("")
+                    || month.getText().equals("")) {
+                return;
+            }
+
             int yearValue = Integer.parseInt(year.getText());
             int monthValue = Integer.parseInt(month.getText());
-            if (year.getText().equals("")
-                    || month.getText().equals("")
-                    || monthValue < 1
-                    || monthValue > 12) {
+
+            if (Integer.parseInt(year.getText()) < 1
+                    || Integer.parseInt(month.getText()) > 12) {
                 return;
             }
 
@@ -61,6 +73,8 @@ public class AddFileScreen {
 
             stage.close();
         });
+        submit.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(submit, Priority.ALWAYS);
 
         root.getChildren().addAll(inputs, submit);
 
